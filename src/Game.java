791e6867e0fefbbd99d1275d1772056196df3741
Constantin-Alin <clmalex99@gmail.com>
2025-01-24 15:35:05 +0100

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,10 @@
+public class Game {
+    public static void main(String[] args) {
+        // Task 1: Afiseaza un mesaj de welcome, ex: "Welcome to Hangman!"
+        // Task 2: Salveaza un cuvant intr-o variabila, ex: airplane
+        // Task 3: Salveaza si afiseaza atatea stelute cate trebuie
+        // Task 4: Cere de la user o litera
+        // Task 5: Verifica daca litera introdusa exista in cuvant si inlocuieste steluta din Array la indexul respectiv
+        // Task 6: Introdu un loop care repeta de la task-ul 4
+    }
+}
